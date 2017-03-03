864bdc263a2361be957fa63a11c001520c07e760
BiG.KMITL <templeboy1992@gmail.com>
2017-03-04 01:57:14 +0700

    - Added function for delete todo.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,16 +2,22 @@
   <div>
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo  v-for="todo in todos" :todo.sync="todo"></todo>
+    <todo  v-on:delete-todo="deleteTodo" v-for="todo in todos" :todo.sync="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
 import Todo from './Todo';
 export default {
-     props: ['todos'],
+  props: ['todos'],
   components: {
     Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    }, 
   },
 };
 </script>
